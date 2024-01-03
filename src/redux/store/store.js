b7bd63dd2f1eb012410724bddd175940ea6cdb2a
dev-alan <alanmc021@gmail.com>
2024-01-03 09:32:24 -0300

    First commit

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,7 @@
 // Imports: Dependencies
-//import AsyncStorage from '@react-native-community/async-storage';
-import { AsyncStorage } from "react-native"
+// import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+//import { AsyncStorage } from "react-native"
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
